50eb4fb42144f2f8804ba0cc295d3b7fe55c78f1
Danylo Vanin <danylo.vanin@ukma.edu.ua>
2023-05-08 03:27:17 +0300

    Fixed max iterations

--- constants.py
+++ constants.py
@@ -8,7 +8,7 @@ LOG = True
 # Max number of iterations
 MAX_ITERATIONS = 10000000
 FCONST_MAX_ITERATIONS = 100000
-F512_MAX_ITERATIONS = 300000
+F512_MAX_ITERATIONS = 10000
 
 #
 RUNS_TO_PLOT = 5
@@ -26,7 +26,7 @@ SUCCESSFUL_RUN_OPTIMAL_GENOTYPE_RATE = 90
 DESIRED_GENE_HOMOGENEITY_LEVEL = 0.99
 
 # Number of runs done per each parameter set
-MAX_RUNS = 10 if env == 'test' else 100
+MAX_RUNS = 10 if env == 'test' else 25
 
 # Used to estimate correctness of results: expected - actual <= DELTA for y, SIGMA for x
 SIGMA = 0.01
